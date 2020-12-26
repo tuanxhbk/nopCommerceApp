62cc04c31fa714f3f20491bc3dc6d2dd8272b8b6
Tuan Nguyen <nguyenmanhtuanxh@gmail.com>
2020-12-26 19:28:17 +0700

    update

--- run.bat
+++ run.bat
@@ -1,1 +1,9 @@
+PATH=$WORKSPACE/venv/bin:$HOME/.local/bin:$PATH
+
+if [ ! -d "venv" ]; then
+		pip install virtualenv --user
+        virtualenv venv
+fi
+. venv/bin/activate
+pip install -r requirements.txt
 pytest -s -v --html=./Reports/report.html testCases/ --browser chrome
