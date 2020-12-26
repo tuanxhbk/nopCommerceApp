d27f7f688d303374fe5beb69755b20e66b88ee8c
Tuan Nguyen <nguyenmanhtuanxh@gmail.com>
2020-12-26 20:13:38 +0700

    update

--- run.bat
+++ run.bat
@@ -6,4 +6,4 @@ if [ ! -d "venv" ]; then
 fi
 . venv/bin/activate
 pip install -r requirements.txt
-python -m pytest -s -v --html=./Reports/report.html testCases/ --browser chrome
+pytest -s -v --html=./Reports/report.html testCases/ --browser chrome
